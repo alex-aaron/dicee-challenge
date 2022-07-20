850a65c215bbb0ee3e76cb11d8f039ffaafd3d03
Alex Aaron <alexaaron@Operations-MacBook-Air.local>
2022-07-20 13:15:39 -0500

    removed commented out test code from index.js

--- index.js
+++ index.js
@@ -2,18 +2,6 @@
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-// window.addEventListener('onbeforeload', function(){
-//   document.getElementById('dice1').setAttribute('src', `images/dice${randomNumber1}.png`);
-//   document.getElementById('dice2').setAttribute('src', `images/dice${randomNumber2}.png`);
-//
-//   if (randomNumber1 > randomNumber2){
-//     document.getElementById('title').innerText = 'Player 1 Wins!';
-//   } else if (randomNumber2 > randomNumber1){
-//     document.getElementById('title').innerText = 'Player 2 Wins!';
-//   } else {
-//     document.getElementById('title').innerText = 'Draw!';
-//   }
-// });
 
 document.getElementById('dice1').setAttribute('src', `images/dice${randomNumber1}.png`);
 document.getElementById('dice2').setAttribute('src', `images/dice${randomNumber2}.png`);
